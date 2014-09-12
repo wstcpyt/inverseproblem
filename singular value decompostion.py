1e13c03aee170954340432423bca21c4985d8f11
yutongpang <wstcpyt@gmail.com>
2014-09-12 16:57:15 -0400

    yutong

--- singular value decompostion.py
+++ singular value decompostion.py
@@ -4,8 +4,8 @@ class SVD():
     def __init__(self,n):
         self.n=n
     def kernel(self,s,t):  #expression of the kernel for the first type Fredholm integral equation of the first kind
-        d=1
-        value = d/(d**2+(s-t)**2)**(3/2)
+        d=0.25
+        value = d*(d**2+(s-t)**2)**(-3/2)
         return value
     def am(self):  # calculate the matrix expression for the operater
         n=self.n
@@ -16,7 +16,7 @@ class SVD():
         AM = np.zeros(s)
         for i in range(0,n):
             for j in range(0,n):
-                AM[i,j] = self.kernel(sj[i],tj[j])
+                AM[i,j] = self.kernel(sj[i],tj[j])/n
         return AM
 
 
@@ -25,7 +25,16 @@ class SVD():
 svdmatrix=SVD(64)
 AM=svdmatrix.am()
 U, s, V =np.linalg.svd(AM,full_matrices=True)
-print(s)
+
+# exact b function
+j = np.arange(1,64+1,dtype=np.float)
+sj = (j-1/2)/64
+b = -60*(sj-0.5)**2+30
+ut =U.T
+utb =np.zeros((64,))
+for i in range(0,64):
+    utb[i] = abs(np.dot(ut[i],b))
+print(ut)
 
 import pylab
 import matplotlib.pyplot as plt
@@ -33,6 +42,6 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.set_yscale('log')
 x = range(1,65)
-plt.scatter(x,s)
+plt.scatter(x,utb)
 plt.show()
 
